1c9cb05a7f4f0141cdae47958d37762602dd032f
Adithyan Anil <adithyananil2002@gmail.com>
2023-10-25 20:06:45 +0530

    added lightshow

--- src/App.js
+++ src/App.js
@@ -1,33 +1,51 @@
-import { useState } from "react";
-import "./App.css";
+import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   const [bgColor, setBgColor] = useState("purple");
-  const HandleBg = (e) => {
-    e.preventDefault();
-    const value = e.target.value;
-    // console.log(value);
- setBgColor(value)
+  const [isChanging, setIsChanging] = useState(false);
+
+  const HandleBg = (color) => {
+    setBgColor(color);
   };
+
+  const setRandomColor = () => {
+    setIsChanging(true);
+  };
+
+  useEffect(() => {
+    if (isChanging) {
+      const intervalId = setInterval(() => {
+        const randomColor = getRandomColor();
+        setBgColor(randomColor);
+      }, 100);
+
+      return () => {
+        clearInterval(intervalId);
+        setIsChanging(false);
+      };
+    }
+  }, [isChanging]);
+
+  const getRandomColor = () => {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
   return (
-    <div style={{backgroundColor:bgColor}} className="App">
-      <h1>BackGround ColorSwitch</h1>
-      <div className="btn-grp">
-        <button value="red" onClick={(e) => HandleBg(e)} className="btn red">
-          Red
-        </button>
-        <button value="blue"  onClick={(e) => HandleBg(e)} className="btn blue">
-          Blue
-        </button>
-        <button value="green" onClick={(e) => HandleBg(e)} className="btn green">
-          Green
-        </button>
-        <button value="black" onClick={(e) => HandleBg(e)} className="btn black">
-          Black
-        </button>
-        <button value="yellow" onClick={(e) => HandleBg(e)} className="btn yellow">
-          Yellow
-        </button>
+    <div style={{ backgroundColor: bgColor }} className="App">
+      <h1>Background Color Switch</h1>
+      <div className='btn-grp'>
+        <button onClick={() => HandleBg('red')} className="btn">Red</button>
+        <button onClick={() => HandleBg('blue')} className="btn">Blue</button>
+        <button onClick={() => HandleBg('green')} className="btn">Green</button>
+        <button onClick={() => HandleBg('black')} className="btn">Black</button>
+        <button onClick={() => HandleBg('yellow')} className="btn">Yellow</button>
+       {isChanging ?  <button onClick={()=>setIsChanging(false)} className="btn">Stop</button>: <button onClick={setRandomColor} className="btn">Light Show</button>}
       </div>
     </div>
   );
